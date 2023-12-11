27e1b3f75b21d26633a011ac7de148243e006f35
sgaidhani63 <sgaidhani63@gmail.com>
2023-12-11 23:29:53 +0530

    test image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(  
+
+    <div>
+      <h1> Meeting Room to discuss with clients </h1>
+      <img src ="https://img.gva.be/3ECGoNgK1T9XCe9kvXh2a76FFKg=/fit-in/1280x853/https%3A%2F%2Fstatic.gva.be%2FAssets%2FImages_Upload%2F2022%2F12%2F20%2F7c4a79e1-c5b5-458f-90e2-30db135f8e35.jpg" alt="sagar"/>
+          </div>
+      
+      )
 }
-
 export default App;
+
+
